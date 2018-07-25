6fd0bc9e3db12e7f30c0a5bc0adf87154a4e7a8d
Ortiz14 <Ortizalberto259@gmail.com>
2018-07-25 08:55:49 -0600

    installed redux-thunl and axios. built auth actions

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -3,10 +3,15 @@ import React, { Component } from 'react';
 import SignupForm from './signupForm';
 
 class Signup extends Component {
+
+  onSubmit = (fields) => {
+    console.log('trying to submit', fields);
+  }
+
   render() {
     return (
       <div className='sign-up'>
-        <SignupForm/>
+        <SignupForm onSubmit={() => this.onSubmit()}/>
       </div>
     )
   }
