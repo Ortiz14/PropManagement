08900095a61f441634a27ab4e513b78df70e216e
Ortiz14 <Ortizalberto259@gmail.com>
2018-07-25 11:34:44 -0600

    build sigin functionality

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 import { ROOT_URL } from '../config';
 
-import {ROOT_URL} from '../config';
-
 import {
     AUTHENTICATE_USER
 } from './types';
@@ -12,8 +10,26 @@ export function signUp(fields, success) {
     return function(dispatch) {
         axios.post(`${ROOT_URL}/signUp`, fields)
             .then(response => {
-                const {token} = response.data;
-                localStorage.setItem('token', token)
+                const { token } = response.data;
+                localStorage.setItem('token', token);
+               dispatch({
+                   type: AUTHENTICATE_USER,
+                   payload: response.data
+               })
+               success();
+            })
+            .catch(err => {
+                if(err) { console.log(err) }
+            })
+    }
+}
+
+export function signIn(fields, success) {
+    return function(dispatch) {
+        axios.post(`${ROOT_URL}/signIn`, fields)
+            .then(response => {
+                const { token } = response.data;
+                localStorage.setItem('token', token);
                dispatch({
                    type: AUTHENTICATE_USER,
                    payload: response.data
